6187a24ebb6f9df461a09a7376bb7a69373aa596
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-06-01 17:43:03 +0300

    Create a serializers.py file with the PostSerializer for users to post with their id, author, author_username, text, image and oter fields.

--- socialapp/serializers.py
+++ socialapp/serializers.py
@@ -33,3 +33,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_following_count(self, obj):
         return obj.following.count()
     
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+    likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'author_username', 'text', 'image', 'video', 'hashtags', 'likes_count', 'comments_count', 'created_at']
+        read_only_fields = ['author']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
+
+    def get_comments_count(self, obj):
+        return obj.comments.count()
