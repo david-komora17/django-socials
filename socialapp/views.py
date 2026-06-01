d2deaa3705940960a140e995bdf0c47c5e181800
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-06-01 17:49:08 +0300

    Create a views.py file and create a permissions check.

--- socialapp/views.py
+++ socialapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status, views
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import Profile, Post, Comment, PostLike, CommentLike
+from .serializers import UserRegisterSerializer, ProfileSerializer, PostSerializer, CommentSerializer
 
-# Create your views here.
+# Create a permissions check.
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
